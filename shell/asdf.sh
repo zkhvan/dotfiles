457d8556da6a8fe87c4104f605995b37a947ef79
Zhenya Khvan <zhenya.khvan@form3.tech>
2023-03-02 14:41:50 -0500

    Add .tool-versions from asdf

--- shell/asdf.sh
+++ shell/asdf.sh
@@ -8,7 +8,7 @@ export KZ_SOURCE="${KZ_SOURCE} -> shell/asdf.sh {"
 
 export ASDF_DIR="${XDG_DATA_HOME}/asdf"
 export ASDF_DATA_DIR="${ASDF_DIR}"
-export ASDF_TOOL_VERSIONS_FILENAME=${XDG_CONFIG_HOME}/asdf/tool-versions
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=".tool-versions"
 
 __kz_source ${ASDF_DIR}/asdf.sh
 
