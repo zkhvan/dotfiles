0f9771f9894cb9ebd4d87bf8d4943be82fc937d6
Zhenya Khvan <zhenya.khvan@gmail.com>
2023-12-12 23:00:14 -0500

    Add kubectl/krew settings

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -29,6 +29,7 @@ alias tree='tree -CF'
 # ansible
 # ----------------------------------------------------------------------------
 
+alias a=ansible
 alias ap=ansible-playbook
 
 # ----------------------------------------------------------------------------
@@ -70,6 +71,10 @@ alias grep='grep --color=auto'
 # ----------------------------------------------------------------------------
 
 alias k='kubectl'
+
+# short alias to set/show context/namespace (only works for bash and bash-compatible shells, current context to be set before using kn to set namespace)
+# alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
+# alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
 
 # ----------------------------------------------------------------------------
 # ssh
---
+++ shell/k8s.sh
@@ -0,0 +1,52 @@
+# shell/k8s.sh
+
+export KZ_SOURCE="${KZ_SOURCE} -> shell/k8s.sh {"
+
+# ============================================================================
+# kubectl helper functions
+# ============================================================================
+
+kx() {
+  if [ "$1" ]; then
+    kubectl config use-context $1
+  else
+    kubectl config current-context
+  fi
+}
+
+kn() {
+  if [ "$1" ]; then
+    kubectl config set-context --current --namespace $1
+  else
+    kubectl config view --minify | grep namespace | cut -d" " -f6
+  fi
+}
+
+# ============================================================================
+# kubectl plugins
+# ============================================================================
+
+_install_krew_plugins() {
+  _plugins=(
+    'get-all' # get's ALL the resources
+  )
+
+  for plugin in "${_plugins[@]}"; do
+    if [[ ! -d "${KREW_ROOT}/store/${plugin}" ]]; then
+      kubectl krew install "${plugin}"
+    fi
+  done
+}
+
+export KREW_ROOT=$XDG_CONFIG_HOME/krew
+export KREW_NO_UPGRADE_CHECK=1
+
+if [[ -d "${KREW_ROOT}" ]]; then
+  PATH="${KREW_ROOT}/bin:${PATH}"
+
+  _install_krew_plugins
+fi
+
+# ============================================================================
+
+KZ_SOURCE="${KZ_SOURCE} }"
