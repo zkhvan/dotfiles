e00b6aa177b036348308c98d384f3a7b43ea9f1e
Zhenya Khvan <zhenya.khvan@form3.tech>
2024-02-14 13:44:18 -0500

    Close floating windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -56,6 +56,16 @@ vim.cmd([[
   endif
 ]])
 
+vim.api.nvim_create_user_command('CloseFloatingWindows', function(opts)
+    for _, window_id in ipairs(vim.api.nvim_list_wins()) do
+        -- If window is floating
+        if vim.api.nvim_win_get_config(window_id).relative ~= '' then
+            -- Force close if called with !
+            vim.api.nvim_win_close(window_id, opts.bang)
+        end
+    end
+end, { bang = true, nargs = 0 })
+
 -- ============================================================================
 -- Security
 -- ============================================================================
