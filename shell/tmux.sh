54939b45bd0fc2885045f7fbfefd5f9f78fec6bf
Zhenya Khvan <zhenya.khvan@form3.tech>
2024-02-27 11:32:28 -0500

    Update tmux layouts

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -21,20 +21,25 @@ tmux-default() {
 # tmux layout helpers
 # ----------------------------------------------------------------------------
 
-tmux-layout() {
+tmux-layout-half-1() {
   tmux split-window -t 1                -v -p 80 -d
   tmux split-window -t "{top}"          -h -p 50 -d
-  tmux split-window -t "{bottom}"       -h -p 90
-  tmux split-window -t "{bottom-right}" -h -p 33 -d
   tmux split-window -t "{bottom}"       -v -p 20 -d
 }
 
-tmux-select-layout5() {
-  tmux select-layout "84fa,478x86,0,0[478x17,0,0{239x17,0,0,174,238x17,240,0,181},478x68,0,18{47x68,0,18,176,288x68,48,18,177,141x68,337,18,180}]"
+tmux-layout-wide-1() {
+  tmux split-window -t 1                -v -p 80 -d
+  tmux split-window -t "{top}"          -h -p 50 -d
+  tmux split-window -t "{bottom}"       -h -p 33 -d
+  tmux split-window -t "{bottom-left}"  -v -p 20 -d
 }
 
-tmux-select-layout6() {
-  tmux select-layout "6eb8,478x86,0,0[478x17,0,0{239x17,0,0,174,238x17,240,0,181},478x68,0,18{47x68,0,18,176,288x68,48,18[288x54,48,18,177,288x13,48,73,185],141x68,337,18,180}]"
+tmux-select-layout-half-1() {
+  tmux select-layout "dd9a,238x81,0,0[238x16,0,0{118x16,0,0,27,119x16,119,0,29},238x51,0,17,28,238x12,0,69,37]"
+}
+
+tmux-select-layout-wide-1() {
+  tmux select-layout "ab31,478x81,0,0[478x16,0,0{238x16,0,0,38,239x16,239,0,40},478x64,0,17{320x64,0,17[320x51,0,17,39,320x12,0,69,42],157x64,321,17,41}]"
 }
 
 # ============================================================================
