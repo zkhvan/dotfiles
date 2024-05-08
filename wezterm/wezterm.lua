b5ccd9618ef134b1ab5e6a3214fdf8722d78537d
Zhenya Khvan <zhenya.khvan@gmail.com>
2024-05-08 00:54:24 -0400

    Style settings for lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,7 @@ scheme.ansi[1] = '#1d1f21'
 config.color_schemes = {
   ['Tomorrow Night Dark (Gogh)'] = scheme,
 }
-config.color_scheme = 'Tomorrow Night Dark (Gogh)';
+config.color_scheme = 'Tomorrow Night Dark (Gogh)'
 
 config.default_cursor_style = 'SteadyBlock'
 config.window_decorations = 'RESIZE'
@@ -22,7 +22,7 @@ config.window_padding = {
 }
 
 -- Fix double-wide nerd fonts when using builtin Nerd Font Symbols
-config.allow_square_glyphs_to_overflow_width = "Never"
+config.allow_square_glyphs_to_overflow_width = 'Never'
 
 config.adjust_window_size_when_changing_font_size = false
 
@@ -30,13 +30,13 @@ config.command_palette_font_size = 16.0
 
 config.font = wezterm.font_with_fallback({
   {
-    family = "JetBrains Mono",
+    family = 'JetBrains Mono',
     scale = 1.2,
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   },
 
   -- emoji fallback 👉 👀 😁 💩 ✅
-  "Noto Color Emoji",
+  'Noto Color Emoji',
 })
 config.font_size = 12.0
 -- config.line_height = 1.24
@@ -50,10 +50,10 @@ config.keys = {
   {
     key = 'Escape',
     mods = 'SHIFT',
-    action = wezterm.action.SendKey {
+    action = wezterm.action.SendKey({
       key = 'F13',
-    }
-  }
+    }),
+  },
 }
 
 config.debug_key_events = true
