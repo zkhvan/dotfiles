667b8bb620d58da56d2cd98bbf217de4501b50f5
Zhenya Khvan <zhenya.khvan@gmail.com>
2024-08-01 21:57:02 -0400

    Yabai config updates

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -2,41 +2,39 @@
 -- init.lua
 -- ---------------------------------------------------------------------------
 
-print("======================================================================")
-
+print('======================================================================')
 
 hs.autoLaunch(true)
 hs.consoleOnTop(false)
 hs.dockIcon(false)
 hs.menuIcon(true)
 
-hyper = {"⌘", "⌃", "⇧"}
-althyper = {"⌘", "⌃", "⇧", "alt"}
+hyper = { '⌘', '⌃', '⇧' }
+althyper = { '⌘', '⌃', '⇧', 'alt' }
 
 local mods = {}
-table.insert(mods, require("clipboard.type"))
-table.insert(mods, require("launcher.seal"))
-table.insert(mods, require("launcher.apps"))
-table.insert(mods, require("menubar.caffeine"))
-table.insert(mods, require("window.gridy"))
-table.insert(mods, require("window.throw"))
-table.insert(mods, require("window.split"))
+table.insert(mods, require('clipboard.type'))
+-- table.insert(mods, require("launcher.seal"))
+-- table.insert(mods, require("launcher.apps"))
+table.insert(mods, require('menubar.caffeine'))
+table.insert(mods, require('window.gridy'))
+table.insert(mods, require('window.throw'))
+table.insert(mods, require('window.split'))
 
-hs.loadSpoon("HCalendar")
+hs.loadSpoon('HCalendar')
 
 -- Regenerate lua annotations
 -- hs.loadSpoon("EmmyLua")
 
-print "== reload"
-hs.hotkey.bind(hyper, "0", function()
+print('== reload')
+hs.hotkey.bind(hyper, '0', function()
   for _, mod in ipairs(mods) do
-    if type(mod) == "table" and mod["destructor"] then
-      print("== destroying " .. mod["name"])
-      mod["destructor"]()
+    if type(mod) == 'table' and mod['destructor'] then
+      print('== destroying ' .. mod['name'])
+      mod['destructor']()
     end
   end
-  hs.notify.show("Reloading Hammerspoon config", "Manually", "")
-  print("reloading...")
+  hs.notify.show('Reloading Hammerspoon config', 'Manually', '')
+  print('reloading...')
   hs.reload()
 end)
-
