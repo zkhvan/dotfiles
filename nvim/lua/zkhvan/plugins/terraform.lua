24957970c9e707d98b6ab017e31ade998d731b12
Zhenya Khvan <zhenya@gptzero.me>
2026-05-29 16:34:35 -0400

    Add datadog provider to terraform doc lookup

--- nvim/lua/zkhvan/plugins/terraform.lua
+++ nvim/lua/zkhvan/plugins/terraform.lua
@@ -9,12 +9,10 @@ return {
       doc.setup(opts)
       -- The plugin doesn't support configuring providers through config.
       -- https://github.com/Afourcat/treesitter-terraform-doc.nvim/blob/e959c1f19f12642da05587443523dc09cf7991a8/lua/treesitter-terraform-doc/init.lua#L217-L225
-      table.insert(doc.providers,
-        {
-          prefix = "pagerduty",
-          name = "pagerduty",
-        }
-      )
+      vim.list_extend(doc.providers, {
+        { prefix = 'pagerduty', name = 'pagerduty' },
+        { prefix = 'datadog', name = 'datadog' },
+      })
     end,
   },
 }
