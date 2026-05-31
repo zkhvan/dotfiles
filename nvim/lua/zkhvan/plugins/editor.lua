47e8494979a9ef6fd8e6d77640e5aa45b285e857
Zhenya Khvan <zhenya@gptzero.me>
2026-05-31 00:13:07 -0400

    Add yanky.nvim and migrate treesitter to main branch
    Add gbprod/yanky.nvim for yank history with cycling after put. Switch
    nvim-treesitter to the main branch, replacing the modules-based config
    with on-demand parser installation driven by a FileType autocommand.

--- nvim/lua/zkhvan/plugins/editor.lua
+++ nvim/lua/zkhvan/plugins/editor.lua
@@ -20,4 +20,22 @@ return {
   {
     'romainl/vim-qf',
   },
+
+  {
+    -- Yank ring: records yanks/deletes and lets you cycle through history
+    -- after a put with <C-p>/<C-n>.
+    'gbprod/yanky.nvim',
+    opts = {},
+    keys = {
+      { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank text' },
+      { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put after cursor' },
+      { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put before cursor' },
+      { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'Put after selection' },
+      { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'Put before selection' },
+      { '<C-p>', '<Plug>(YankyPreviousEntry)', desc = 'Cycle to previous yank' },
+      { '<C-n>', '<Plug>(YankyNextEntry)', desc = 'Cycle to next yank' },
+      { ']p', '<Plug>(YankyPutIndentAfterLinewise)', desc = 'Put indented after (linewise)' },
+      { '[p', '<Plug>(YankyPutIndentBeforeLinewise)', desc = 'Put indented before (linewise)' },
+    },
+  },
 }
--- nvim/lua/zkhvan/plugins/editor_treesitter.lua
+++ nvim/lua/zkhvan/plugins/editor_treesitter.lua
@@ -3,60 +3,85 @@ local FT_TO_LANG_ALIASES = {
   tiltfile = 'starlark',
 }
 
+local ENSURE_INSTALLED = {
+  'lua',
+  'luadoc',
+  'vim',
+  'vimdoc',
+}
+
 -- blacklist if highlighting doesn't look right or is worse than vim regex
 local HIGHLIGHTING_DISABLED = {
   -- treesitter language, not ft
-  -- see https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+  -- see https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
   'dockerfile',
 }
+
+local installing = {}
+
+local function maybe_install_parser(lang)
+  if installing[lang] then
+    return
+  end
+
+  local installed = require('nvim-treesitter').get_installed()
+  if vim.tbl_contains(installed, lang) then
+    return
+  end
+
+  local available = require('nvim-treesitter').get_available()
+  if not vim.tbl_contains(available, lang) then
+    return
+  end
+
+  installing[lang] = true
+  require('nvim-treesitter').install(lang)
+end
+
+local function start_treesitter(args)
+  local ft = vim.bo[args.buf].filetype
+  local lang = vim.treesitter.language.get_lang(ft)
+  if not lang then
+    return
+  end
+
+  local has_parser = pcall(vim.treesitter.get_parser, args.buf, lang)
+  if not has_parser then
+    maybe_install_parser(lang)
+    return
+  end
+
+  if not vim.tbl_contains(HIGHLIGHTING_DISABLED, lang) then
+    pcall(vim.treesitter.start, args.buf, lang)
+  end
+
+  vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
 
 --- @module 'lazy'
 --- @type LazySpec[]
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPost', 'BufNewFile' },
-    cmd = { 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
+    branch = 'main',
+    lazy = false,
     build = function()
-      require('nvim-treesitter.install').update({ with_sync = true })
+      local treesitter = require('nvim-treesitter')
+      treesitter.install(ENSURE_INSTALLED):wait(300000)
+      treesitter.update():wait(300000)
     end,
     config = function()
-      require('nvim-treesitter.configs').setup({
-        -- A list of parser names
-        ensure_installed = {
-          'lua',
-          'luadoc',
-          'vim',
-          'vimdoc',
-        },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        ignore_install = {},
-
-        modules = {},
-
-        highlight = {
-          enable = true,
-          disable = function(lang, _)
-            return (vim.tbl_contains(HIGHLIGHTING_DISABLED, lang))
-          end,
-        },
-
-        indent = {
-          enable = true,
-        },
-      })
+      require('nvim-treesitter').setup()
 
       -- Aliases
       for ft, parser in pairs(FT_TO_LANG_ALIASES) do
         vim.treesitter.language.register(parser, ft)
       end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('zkhvan_treesitter', { clear = true }),
+        callback = start_treesitter,
+      })
     end,
   },
 }
